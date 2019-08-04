d8a3fb8791def283b64cd61fc80e29f7aa093c60
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-08-04 19:06:21 -0300

    Adicionando clientes

---
+++ src/main/java/br/com/atena/service/ClienteService.java
@@ -0,0 +1,11 @@
+package br.com.atena.service;
+
+import br.com.atena.commons.DefaultService;
+import br.com.atena.model.CategoriaModel;
+import br.com.atena.repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService extends DefaultService<CategoriaModel,CategoriaRepository> {
+
+}
